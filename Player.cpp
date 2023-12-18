b2e3d08ca0c4e16d22bb44d182b0a1efc0fa7608
funmilayo <funmilayo_hamed@yahoo.com>
2023-12-18 20:14:39 +0000

    This is the fundamental part of the program

---
+++ Player.cpp
@@ -0,0 +1,19 @@
+// Class representing a player in a card game
+class Player {
+public:
+    string playerName;  // Name of the player
+    int cardAtHand;     // Card currently held by the player
+    int score;          // Player's score in the game
+
+    // Function to make the player take a card
+    void takeCard(int card) {
+        // This function sets the card at hand to the provided card
+        cardAtHand = card;
+    }
+
+    // Function to get the player's score
+    int getScore() {
+        // Returns the player's score
+        return score;
+    }
+};
