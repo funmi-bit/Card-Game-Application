eac5909ce55b49dee744e38eac4cc0edb0af9869
funmilayo <funmilayo_hamed@yahoo.com>
2023-12-19 07:07:39 +0000

    player and deck headrer files is included to card game header file

--- CardGame.cpp
+++ CardGame.cpp
@@ -1,19 +1,12 @@
-// Class representing a card game
-class CardGame {
-public:
-    int deck;            // Number of decks in the game
-    int players;         // Number of players in the game
-    int currentRound;    // Current round of the game
+#include "CardGame.h"
+#include "Player.h"
+#include <cstdlib>
+#include <ctime>
 
-    // Function to start the game
-    void startGame() {}
+using namespace std;
 
-    // Function to play a round of the game
-    void playRound() {}
-
-    // Function to determine the winner of the game
-    int determineWinner() { return 0; }
-
-    // Function to progress the game to the next state
-    void progressGame() {}
-};
+CardGame::CardGame() : currentRound(0) {
+    players[0] = new ConcretePlayer("PlayerOne");
+    players[1] = new ConcretePlayer("PlayerTwo");
+    players[2] = new ConcretePlayer("PlayerThree");
+}
