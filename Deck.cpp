35ee213912abece5c0547d783f959350cc58932e
funmilayo <funmilayo_hamed@yahoo.com>
2023-12-19 08:04:29 +0000

    The  method finds and marks a specific card as dealt within the deck, and  counts and returns the number of undealt cards in the deck.

--- Deck.cpp
+++ Deck.cpp
@@ -12,3 +12,21 @@ void Deck::shuffle() {
     std::shuffle(std::begin(deckCards),std::end(deckCards), rng);
 }
 
+void Deck::dealCard(int card) {
+    for (int i = 0; i < 50; ++i) {
+        if (deckCards[i] == card) {
+            deckCards[i] = -1;
+            break;
+        }
+    }
+}
+
+int Deck::getRemainingCards() {
+    int count = 0;
+    for (int i = 0; i < 50; ++i) {
+        if (deckCards[i] != -1) {
+            count++;
+        }
+    }
+    return count;
+}
