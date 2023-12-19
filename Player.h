154365881906e62c7a3bed09258f090cf7a6c085
funmilayo <funmilayo_hamed@yahoo.com>
2023-12-19 08:51:59 +0000

    Abstract class Player defines virtual functions for managing cards, score, name, and has a virtual destructor.~

--- Player.h
+++ Player.h
@@ -2,3 +2,12 @@
 #define PLAYER_H
 
 #include <string>
+
+class Player {
+public:
+    virtual void takeCard(int card) = 0;
+    virtual int getScore() const = 0;
+    virtual std::string getPlayerName() const = 0;
+    virtual void setPlayerName(const std::string& name) = 0;
+    virtual ~Player() {}
+};
