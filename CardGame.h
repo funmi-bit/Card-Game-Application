282b09d944e55b3823841f61e48bc142bcd6f798
funmilayo <funmilayo_hamed@yahoo.com>
2023-12-19 07:21:12 +0000

    The CardGame class manages a card game with rounds, a deck, and three players. It has methods to start, play, and determine the winner

--- CardGame.h
+++ CardGame.h
@@ -3,3 +3,18 @@
 #include <iostream>
 #include "Player.h"
 #include "Deck.h"
+class CardGame {
+public:
+    int currentRound;
+    Deck myDeck;
+    Player* players[3];
+
+    CardGame();
+    void startGame();
+    void playRound();
+
+    void determineWinner();
+    ~CardGame();
+};
+
+#endif // CARDGAME_H
