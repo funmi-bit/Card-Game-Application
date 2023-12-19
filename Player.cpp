8fe29506877d3cbb3c4102902d473e2fa41afe66
funmilayo <funmilayo_hamed@yahoo.com>
2023-12-19 08:41:28 +0000

    Defines a concrete player class with a constructor, methods to manage cards and score, and functions to access and modify player information.

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,19 @@
 #include "Player.h"
+ConcretePlayer::ConcretePlayer(const std::string& name) : playerName(name), cardAtHand(0), score(0) {}
+
+void ConcretePlayer::takeCard(int card) {
+    cardAtHand = card;
+    score += card;
+}
+
+int ConcretePlayer::getScore() const {
+    return score;
+}
+
+std::string ConcretePlayer::getPlayerName() const {
+    return playerName;
+}
+
+void ConcretePlayer::setPlayerName(const std::string& name) {
+    playerName = name;
+}
