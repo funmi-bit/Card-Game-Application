3143b9111738d302bbb930e32228745f2157dfcf
funmilayo <funmilayo_hamed@yahoo.com>
2023-12-27 08:27:08 +0000

    the game was modified to allow user input. this interation enables playerOne to choose their card and the remaining  two players are automated.

--- CardGame.cpp
+++ CardGame.cpp
@@ -1,19 +1,22 @@
+// CardGame.cpp
 #include "CardGame.h"
-#include "Player.h"
-#include <cstdlib>
-#include <ctime>
 
 using namespace std;
 
+// Constructor initializes the game with three players and sets the current round to 0
 CardGame::CardGame() : currentRound(0) {
     players[0] = new ConcretePlayer("PlayerOne");
     players[1] = new ConcretePlayer("PlayerTwo");
     players[2] = new ConcretePlayer("PlayerThree");
 }
+
+// Starts the game by shuffling the deck and initiating the first round
 void CardGame::startGame() {
     myDeck.shuffle();
     playRound();
 }
+
+// Handles the logic for each round of the game
 void CardGame::playRound() {
     for (int i = 0; i < 3; ++i) {
         int randomIndex = rand() % 50;
@@ -21,12 +24,32 @@ void CardGame::playRound() {
 
         if (myDeck.deckCards[randomIndex] != -1) {
             myDeck.dealCard(randomCard);
-            players[i]->takeCard(randomCard);
+
+            if (i == 0) {
+                // PlayerOne picks a card
+                cout << players[i]->getPlayerName() << ", choose a card from 0 to 9: ";
+                int userSelectedCard;
+                cin >> userSelectedCard;
+
+                // Validate user input for playerOne
+                if (userSelectedCard >= 0 && userSelectedCard <= 9) {
+                    players[i]->takeCard(userSelectedCard);
+                    cout << players[i]->getPlayerName() << " has chosen card " << userSelectedCard << endl;
+                } else {
+                    cout << "Invalid card selection. Please choose a card from 0 to 9." << endl;
+                    // If input is invalid, decrement the loop counter to repeat the iteration for the same player
+                    --i;
+                }
+            } else {
+                // PlayerTwo and PlayerThree get a random card
+                int randomCardForOtherPlayers = rand() % 10;
+                players[i]->takeCard(randomCardForOtherPlayers);
+                cout << players[i]->getPlayerName() << " has chosen card " << randomCardForOtherPlayers << endl;
+            }
 
             int remainingCards = myDeck.getRemainingCards();
-          cout << players[i]->getPlayerName() << " has number " << randomCard << " card at hand" << endl;
             cout << "Remaining cards in the deck: " << remainingCards << endl << endl << endl << endl;
-            
+
         } else {
             cout << "No cards left in the deck." << endl;
         }
@@ -40,6 +63,7 @@ void CardGame::playRound() {
     }
 }
 
+// Determines the winner of the game based on the players' scores
 void CardGame::determineWinner() {
     int maxScore = -1;
     int winnerIndex = -1;
@@ -53,13 +77,17 @@ void CardGame::determineWinner() {
             winnerIndex = i;
         }
     }
-        cout << "---------------Game Over-------------" << endl;
+
+    cout << "---------------Game Over-------------" << endl;
     cout << "Winner: " << players[winnerIndex]->getPlayerName() << endl;
-      // Clean up memory
+
+    // Clean up memory
     for (int i = 0; i < 3; ++i) {
         delete players[i];
     }
 }
+
+// Destructor
 CardGame::~CardGame() {
-  
+    
 }
--- CardGame.h
+++ CardGame.h
@@ -1,20 +1,23 @@
+// CardGame.h
 #ifndef CARDGAME_H
 #define CARDGAME_H
+
 #include <iostream>
 #include "Player.h"
 #include "Deck.h"
+
+// Class representing a card game
 class CardGame {
 public:
     int currentRound;
     Deck myDeck;
     Player* players[3];
 
-    CardGame();
+    CardGame();  // Constructor
     void startGame();
     void playRound();
-
     void determineWinner();
-    ~CardGame();
+    ~CardGame();  // Destructor
 };
 
 #endif // CARDGAME_H
